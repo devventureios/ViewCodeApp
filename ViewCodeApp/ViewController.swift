45183ea24ddddf3c4534dab9741e7e934ae766ef
Eric Brito <eric_brito@msn.com>
2020-06-01 19:08:17 -0300

    Initial Commit

---
+++ ViewCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewCodeApp
+//
+//  Created by Eric Alves Brito on 01/06/20.
+//  Copyright © 2020 DevVenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
